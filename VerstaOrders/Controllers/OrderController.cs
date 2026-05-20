d667f3ec4097fa7178c887396f340d78741988fc
Turov Andrey <andrey.turoff@gmail.com>
2026-05-20 15:45:21 +0300

    init

---
+++ VerstaOrders/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VerstaOrders.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok("Order");
+    }
+}
---
+++ VerstaOrders/Model/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace VerstaOrders.Model;
+
+public record Order(
+    Guid OrderId,
+    string TownSender,
+    string AddressSender,
+    string TownReceiver,
+    string AddressReceiver,
+    double ProductWeight,
+    DateTime PickupDate);
